f5501e5997f0b252cd98aac7fabc460c933769aa
dgusa <dgus@tut.by>
2019-10-20 02:23:05 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max=max;
+        this.min=min;
+        
     }
 
     guess() {
-
+        
+        this.number=Math.round((this.max+this.min)/2);
+     return this.number;
     }
 
     lower() {
-
+       this.max=this.number;
+        
     }
 
     greater() {
-
+       this.min=this.number;
     }
 }
 
